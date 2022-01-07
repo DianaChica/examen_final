774f78c82b73cb2d707bdbe332ec3f33b1f7a004
eeestupi <eeestupi@espol.edu.ec>
2022-01-07 10:17:32 -0500

    update mongo

--- frontend-main/models/routes.js
+++ frontend-main/models/routes.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/router_db', {   
-useNewUrlParser: true,
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/router_db', {
+    useNewUrlParser: true,
 },
+
 (err) => {
     if (!err) {
         console.log('Connection suceeded');
