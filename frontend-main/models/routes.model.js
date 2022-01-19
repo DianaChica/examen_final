1972185f7cdf9fd8bf948fa89bc69ed45b4a1347
eeestupi <eeestupi@espol.edu.ec>
2022-01-18 20:06:29 -0500

    1234

--- frontend-main/models/routes.model.js
+++ frontend-main/models/routes.model.js
@@ -1,16 +1,43 @@
 const mongoose = require('mongoose')
 
+// var routerSchema = new mongoose.Schema({
+//     ip: { type: String, required: ' This field is required', },
+//     origin: { type: String, required: ' This field is required', },
+//     metric: { type: String, required: ' This field is required', },
+//     localpref: { type: String, required: ' This field is required', },
+//     valid: { type: String, required: ' This field is required', },
+//     internal: { type: String, required: ' This field is required', },
+//     date: {
+//         type: Date,
+//         required: ' This field is required',
+//     },
+// });
+
+// mongoose.model("Routes", routerSchema);
+
 var routerSchema = new mongoose.Schema({
-    ip: { type: String, required: ' This field is required', },
-    origin: { type: String, required: ' This field is required', },
-    metric: { type: String, required: ' This field is required', },
-    localpref: { type: String, required: ' This field is required', },
-    valid: { type: String, required: ' This field is required', },
-    internal: { type: String, required: ' This field is required', },
-    date: {
-        type: Date,
-        required: ' This field is required',
-    },
+    FuelTypeCode: { type: String, required: ' This field is required', },
+    StationName: { type: String, required: ' This field is required', },
+    StreetAddress: { type: String, required: ' This field is required', },
+    City: { type: String, required: ' This field is required', },
+    State: { type: String, required: ' This field is required', },
+    Zip:{ type: Number, required: ' This field is required', },
+    StatusCode: { type: String, required: ' This field is required', },
+    GroupsWithAccessCode: { type: String, required: ' This field is required', },
+    AccessDaysTime :{ type: String, required: ' This field is required', },
+    EVLevel2EVSENum:{ type: String, required: ' This field is required', },
+    EVDCFastCount:{ type: String, required: ' This field is required', },
+    EVNetwork:{ type: String, required: ' This field is required', },
+    GeocodeStatus:{ type: String, required: ' This field is required', },
+    Latitude:{ type: String, required: ' This field is required', },
+    Longitude:{ type: String, required: ' This field is required', },
+    DateLastConfirmed:{ type: String, required: ' This field is required', },
+    Id:{ type: String, required: ' This field is required', },
+    UpdatedAt:{ type: String, required: ' This field is required', },
+    OwnerTypeCode:{ type: String, required: ' This field is required', },
+    OpenDate:{ type: String, required: ' This field is required', },
+    EVConnectorTypes:{ type: String, required: ' This field is required', },
+    Country:{ type: String, required: ' This field is required', },
 });
 
 mongoose.model("Routes", routerSchema);
